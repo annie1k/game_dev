ab43a7e3d41597405d0fded13c4d7931b9a75247
Annie <1000wangyiqian@gmail.com>
2021-12-30 21:33:49 -0800

    command line game compete with answer

--- tripleX/tripleX.cpp
+++ tripleX/tripleX.cpp
@@ -12,6 +12,7 @@ int a;      uninitialized var
 
 int main() 
 {
+    std::cout << "Annie: \"lol\" \n";
     // expression follow by a ; is a expression statements.
     std::cout << "You are a secret agent breaking into a secure server room...\n";
     // std::cout << std::endl;
@@ -19,18 +20,41 @@ int main()
 
     
     // following are declaration statements
-    const int a = 7;
-    const int b = 8;
-    const int c = 9;
+    const int CodeA = 7;
+    const int CodeB = 8;
+    const int CodeC = 9;
     // a = 100; if already const, then can not change it.
-    const int sum = a + b + c;
-    const int mul = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeMul = CodeA * CodeB * CodeC;
 
     // expression follow by a ; is a expression statements.
     std::cout << "There are 3 numbers in our code." << std::endl;
-    std::cout << "The sum: " << sum << std::endl;
-    std::cout << "The product: " << mul << std::endl;
+    std::cout << "The sum: " << CodeSum << std::endl;
+    std::cout << "The product: " << CodeMul << std::endl;
 
+    int PlayerGuessA, PlayerGuessB, PlayerGuessC;
+    std::cout << "Please enter your guess: \n";
+    // std::cout << "Please enter your guess of A: \n";
+    std::cin >> PlayerGuessA >> PlayerGuessB >> PlayerGuessC;
+    // std::cout << "Please enter your guess of B: \n";
+    // std::cin >> PlayerGuessB;
+    // std::cout << "Please enter your guess of C: \n";
+    // std::cin >> PlayerGuessC;
+    std::cout << "You entered: " << PlayerGuessA << PlayerGuessB << PlayerGuessC << std::endl;
+
+    int CheckSum = PlayerGuessA + PlayerGuessB + PlayerGuessC;
+    int CheckPro = PlayerGuessA * PlayerGuessB * PlayerGuessC;
+
+    if (CheckSum == CodeSum) 
+    {
+        std::cout << "You Win \n";
+    } 
+    else 
+    {
+        std::cout << "You Lose \n";
+    }
     // return statement
+    // if + tab
+    
     return 0;
 }
